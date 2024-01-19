9c8a15fb83fd1df58a24de8dda4a3cd983d82f93
Amruta101998 <hulaguramruta325@gmail.com>
2024-01-19 20:18:44 +0530

    New code updated

--- Java/avg/array.java
+++ Java/avg/array.java
@@ -12,4 +12,15 @@ public class array {
             System.out.println(arr[i]);
         }
     }
+
+    public static void Even(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        System.out.println("Even numbers:");
+
+        for (int i = 0; i < arr.length; i = i * 2)
+        {
+            System.out.println(arr[i]);
+        }
+    }
 }
